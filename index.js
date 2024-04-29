a05c092589edb0bb1c4867be9937a8237f99a5db
Freedom Ohikhena <freedomohikhena@gmail.com>
2024-04-29 14:54:40 -0800

    new project

---
+++ index.js
@@ -0,0 +1,8 @@
+function displayTime(){
+    var d = new Date();
+    var hour = d.getHours();
+    var min = d.getMinutes();
+    var sec = d.getSeconds();
+    document.getElementById("clock").innerHTML = hour + ":" + min + ":" + sec;
+}
+setInterval(displayTime,1000);
